687750b767fe585b18e952611d61f81058d72fe8
Darin <dcrawmer1@gmail.com>
2017-03-06 23:43:40 -0800

    Added Hex simple snap to grid
    Used simple math to snap to grid/board.

--- Assets/Scripts/MovePiece.cs
+++ Assets/Scripts/MovePiece.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class MovePiece : MonoBehaviour {
 	
 	public Vector3 targetPosition;
+	public float x;
+	public float y;
 
 	// Update is called once per frame
 	void Update () {
@@ -12,13 +14,39 @@ public class MovePiece : MonoBehaviour {
 		if(Input.GetKeyDown(KeyCode.Mouse0))
 		{
 			targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			targetPosition.Set(targetPosition.x, targetPosition.y, 0.0f);
+			//targetPosition.Set(targetPosition.x, targetPosition.y, 0.0f);
 			//print(transform.position);
 			//transform.position = targetPosition;   // Imediately Sets Position
+
+
+			x = ((Mathf.Round ((1 / 0.88f) * targetPosition.x)));
+			y = ((Mathf.Round (2 * targetPosition.y)) / 2);
+			if (Mathf.Abs(x % 2) == 0f) {
+				// Even Number
+				Debug.Log("Even number");
+				y = (Mathf.Round(y));
+			}
+				
+			if (Mathf.Abs(x % 2) == 1f) {
+				// Odd number
+				Debug.Log("Odd number");
+				y = (Mathf.Round(y+0.5f)-0.5f);
+			}
+
+			Debug.Log (targetPosition.x);
+			Debug.Log (targetPosition.y);
+			Debug.Log (x);
+			Debug.Log (y);
+
+
+			targetPosition.Set(x * 0.88f, y, 0.0f);
 
 		}
 
 		transform.position = Vector3.MoveTowards(transform.position, targetPosition, Time.deltaTime * 2);
 
+
 	}
+
+
 }
